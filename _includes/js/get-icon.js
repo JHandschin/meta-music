98afcea10786dfee53444e0ad89cfdae2c558d1f
Joshua Handschin <41125028+JHandschin@users.noreply.github.com>
2023-10-27 11:22:12 -0600

    Update get-icon.js

--- _includes/js/get-icon.js
+++ _includes/js/get-icon.js
@@ -12,32 +12,34 @@ function getIcon(objectTemplate,objectFormat,svgType) {
         iconTemplate = ""
     }
     // choose icon
-    iconTemplate = "audio";
-    if (iconTemplate.includes("image")) {
-        iconId = "icon-image";
-        iconTitle = "image file icon";
-    } else if (iconTemplate.includes("pdf")) {
-        iconId = "icon-pdf";
-        iconTitle = "pdf file icon";
-    } else if (iconTemplate.includes("video")) {
-        iconId = "icon-video";
-        iconTitle = "video file icon";
-    } else if (iconTemplate.includes("audio")) {
-        iconId = "icon-audio";
-        iconTitle = "audio file icon";
-    } else if (iconTemplate.includes("panorama")) {
-        iconId = "icon-panorama";
-        iconTitle = "panorama file icon";
-    } else if (iconTemplate.includes("compound")) {
-        iconId = "icon-compound-object";
-        iconTitle = "compound object icon";
-    } else if (iconTemplate.includes("multiple")) {
-        iconId = "icon-multiple";
-        iconTitle = "multiple object icon";
-    } else {
-        iconId = "icon-default";
-        iconTitle = "file icon";
-    }
+    // iconTemplate = "audio";
+    // if (iconTemplate.includes("image")) {
+    //     iconId = "icon-image";
+    //     iconTitle = "image file icon";
+    // } else if (iconTemplate.includes("pdf")) {
+    //     iconId = "icon-pdf";
+    //     iconTitle = "pdf file icon";
+    // } else if (iconTemplate.includes("video")) {
+    //     iconId = "icon-video";
+    //     iconTitle = "video file icon";
+    // } else if (iconTemplate.includes("audio")) {
+    //     iconId = "icon-audio";
+    //     iconTitle = "audio file icon";
+    // } else if (iconTemplate.includes("panorama")) {
+    //     iconId = "icon-panorama";
+    //     iconTitle = "panorama file icon";
+    // } else if (iconTemplate.includes("compound")) {
+    //     iconId = "icon-compound-object";
+    //     iconTitle = "compound object icon";
+    // } else if (iconTemplate.includes("multiple")) {
+    //     iconId = "icon-multiple";
+    //     iconTitle = "multiple object icon";
+    // } else {
+    //     iconId = "icon-default";
+    //     iconTitle = "file icon";
+    // }
+    iconId = "music-svgrepo-com";
+    iconTitle = "audio file icon";
     if (svgType == "thumb") {
         // svg sprite as thumb
         return '<svg class="bi text-body" fill="currentColor" role="img"><title>' + iconTitle + '</title><use xlink:href="{{ "/assets/lib/cb-icons.svg" | relative_url }}#' + iconId + '"/></svg>';
